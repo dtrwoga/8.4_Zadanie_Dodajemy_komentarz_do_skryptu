1f8e44ec1902a65b0a159d098b10e586820d1f13
dtrwoga <dtrwoga@gmail.com>
2017-07-28 00:22:41 +0200

    add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 // scripts.js
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+//alert('Hello, ' + name);
 console.log('Hello, ' + name);
+/*komentarz blokowy
+komentarz blokowy*/
+//komentarz liniowy
